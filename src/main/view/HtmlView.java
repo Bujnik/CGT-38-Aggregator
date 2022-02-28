7752e737017cd9f347b8d5bf82b61c61c32aa95f
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2022-02-28 22:00:38 +0100

    Part 13
    1. In the HtmlView class, create an empty private String getUpdatedFileContents(List<JobPosting>) method.
    2. In the HtmlView class, create an empty private void updateFile(String) method.
    3. Using the created methods, implement the update method in accordance with the task conditions.
    4. In the HtmlView class, create a private final String filePath field and assign it the relative path to jobPostings.html.
    
    Html files provided by CodeGym

--- src/main/view/HtmlView.java
+++ src/main/view/HtmlView.java
@@ -7,11 +7,33 @@ import java.util.List;
 
 public class HtmlView implements View{
     private Controller controller;
+    private final String filePath = "./src/" +
+            this.getClass()
+                    .getPackage()
+                    .toString()
+                    .split(" ")[1]
+                    .replaceAll("\\.", "/")
+            + "/"
+            + "jobPostings.html";
 
     @Override
     public void update(List<JobPosting> jobPostings) {
+        try {
+            String newContent = getUpdatedFileContents(jobPostings);
+            updateFile(newContent);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String getUpdatedFileContents(List<JobPosting> list){
+        return null;
+    }
+
+    private void updateFile(String s){
 
     }
+
 
     @Override
     public void setController(Controller controller) {
