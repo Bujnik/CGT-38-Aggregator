5c21bd8c48cc1050708d031379836137f4f58874
Dawid Bujnicki <89477976+Bujnik@users.noreply.github.com>
2022-03-05 11:39:15 +0100

    Part 15
    Ambigious description of the task made me take getUpdatedFileContents method from CodeGym

--- src/main/view/HtmlView.java
+++ src/main/view/HtmlView.java
@@ -2,7 +2,12 @@ package main.view;
 
 import main.Controller;
 import main.vo.JobPosting;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -10,7 +15,7 @@ import java.util.List;
 
 public class HtmlView implements View{
     private Controller controller;
-    private final String filePath = "./src/" +
+    private final String filePath = "./4.JavaCollections/src/" +
             this.getClass()
                     .getPackage()
                     .toString()
@@ -23,16 +28,51 @@ public class HtmlView implements View{
     public void update(List<JobPosting> jobPostings) {
         try {
             String newContent = getUpdatedFileContents(jobPostings);
+            assert newContent != null;
             updateFile(newContent);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private String getUpdatedFileContents(List<JobPosting> list){
-        return null;
-    }
+    /**
+     * This method I took from codegym as I don't have testing ability for this one
+     */
+    private String getUpdatedFileContents(List<JobPosting> vacancies) {
+        try {
+            Document doc = getDocument();
+            Elements templateHidden = doc.getElementsByClass("template");
+            Element template = templateHidden.clone().removeAttr("style").removeClass("template").get(0);
 
+            //remove all prev vacancies
+            Elements prevVacancies = doc.getElementsByClass("vacancy");
+
+            for (Element redundant : prevVacancies) {
+                if (!redundant.hasClass("template")) {
+                    redundant.remove();
+                }
+            }
+
+            //add new vacancies
+            for (JobPosting vacancy : vacancies) {
+                Element vacancyElement = template.clone();
+
+                Element vacancyLink = vacancyElement.getElementsByAttribute("href").get(0);
+                vacancyLink.appendText(vacancy.getTitle());
+                vacancyLink.attr("href", vacancy.getUrl());
+                Element city = vacancyElement.getElementsByClass("city").get(0);
+                city.appendText(vacancy.getCity());
+                Element companyName = vacancyElement.getElementsByClass("companyName").get(0);
+                companyName.appendText(vacancy.getCompanyName());
+
+                templateHidden.before(vacancyElement.outerHtml());
+            }
+            return doc.html();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "Some exception occurred";
+    }
 
     private void updateFile(String s){
         try (FileOutputStream fos = new FileOutputStream(filePath)){
@@ -40,6 +80,10 @@ public class HtmlView implements View{
         }
         catch (IOException ignored){
         }
+    }
+
+    protected Document getDocument() throws IOException{
+        return Jsoup.parse(new File(filePath), "UTF-8");
     }
 
 
